ceeee6fbd6e9aed87c7984d186ccfaefddf7bb4c
ceduardoz <ceduardoz957@gmail.com>
2026-02-19 23:02:21 -0300

    refactor: update import statements and improve code structure in app.js and server.js

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import './src/database';
-
+// app.js
 import express from "express";
 import { resolve } from "path";
 
-import homeRouter from "./src/routes/homeRoutes";
-import userRouter from "./src/routes/userRoutes";
-import tokenRouter from "./src/routes/tokenRoutes";
-import alunoRouter from "./src/routes/alunoRoutes";
-import photoRouter from "./src/routes/photoRoutes";
+import './src/database'; // se estiver no mesmo nível de src
+
+import homeRouter from "./src/routes/homeRoutes.js";
+import userRouter from "./src/routes/userRoutes.js";
+import tokenRouter from "./src/routes/tokenRoutes.js";
+import alunoRouter from "./src/routes/alunoRoutes.js";
+import photoRouter from "./src/routes/photoRoutes.js";
+
 class App {
-  constructor(){
+  constructor() {
     this.app = express();
     this.middleware();
     this.routes();
   }
 
-  middleware(){
+  middleware() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, "uploads")));
   }
 
-  routes(){
+  routes() {
     this.app.use("/", homeRouter);
-    this.app.use("/users/", userRouter);
-    this.app.use("/token/", tokenRouter);
-    this.app.use("/alunos/", alunoRouter);
-    this.app.use("/fotos/", photoRouter);
+    this.app.use("/users", userRouter);
+    this.app.use("/token", tokenRouter);
+    this.app.use("/alunos", alunoRouter);
+    this.app.use("/fotos", photoRouter);
   }
 }
 
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = process.env.APP_PORT;
+import app from "./app.js";
 
-app.listen(PORT)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
